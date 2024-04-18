63d3ebf8117ff387ab5d45501a863695013594b1
bharathummaluru <bharathummaluru@gmail.com>
2024-04-18 18:38:50 +0530

    conditions

--- 10-installation.sh
+++ 10-installation.sh
@@ -4,17 +4,17 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
- echo"please run this with root user acess"
+ echo "please run this with root user acess"
 else
-  echo"you are a super user"
+  echo "you are a super user"
 fi
 dnf install mysql
 
 if [ $? -ne 0 ]
  then
-  echo"installation of my sql...FAILURE"
+  echo "installation of my sql...FAILURE"
  else
-   echo"installation of my sql...SUCCESS" 
+   echo "installation of my sql...SUCCESS" 
 fi
 
 
