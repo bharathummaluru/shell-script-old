4dcbb78848a6b613e4fcf4e0806846c482367513
bharathummaluru <bharathummaluru@gmail.com>
2024-04-18 18:42:02 +0530

    conditions

--- 10-installation.sh
+++ 10-installation.sh
@@ -5,6 +5,7 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
  echo "please run this with root user acess"
+ exit 1
 else
   echo "you are a super user"
 fi
@@ -13,8 +14,19 @@ dnf install mysql
 if [ $? -ne 0 ]
  then
   echo "installation of my sql...FAILURE"
+  exit 1
  else
    echo "installation of my sql...SUCCESS" 
 fi
+dnf install git -y
+
+if [ $? -ne 0 ]
+then
+    echo "Installation of git...FAILURE"
+    exit 1
+else
+    echo "Installation of Git...SUCCESS"
+fi
+
 
 
